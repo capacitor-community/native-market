532914257148447e458e0b1bb53b8680c8c95e3b
Priyank Patel <priyank.patel@stackspace.ca>
2020-06-21 10:32:40 -0400

    Cleanup

--- src/definitions.ts
+++ src/definitions.ts
@@ -5,9 +5,9 @@ declare module "@capacitor/core" {
 }
 
 export interface NativeMarketPlugin {
-  openStoreListing(options: { appId: string; }): Promise<void>;
-  openDevPage(options: { devId: string; }): Promise<void>;
-  openCollection(options: { name: string; }): Promise<void>;
-  openEditorChoicePage(options: { editorChoice: string; }): Promise<void>;
-  search(options: { terms: string; }): Promise<void>;
+  openStoreListing(options: { appId: string }): Promise<void>;
+  openDevPage(options: { devId: string }): Promise<void>;
+  openCollection(options: { name: string }): Promise<void>;
+  openEditorChoicePage(options: { editorChoice: string }): Promise<void>;
+  search(options: { terms: string }): Promise<void>;
 }
