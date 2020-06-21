162a9148ed38332b38a8c4fd4eaae64d075cfb3b
Priyank Patel <priyank.patel@stackspace.ca>
2020-06-21 10:32:32 -0400

    Cleanup spacings

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,27 +1,26 @@
-import { Plugins } from '@capacitor/core';
-import { NativeMarketPlugin as NMPlugin } from './definitions';
+import { Plugins } from "@capacitor/core";
+import { NativeMarketPlugin as NMPlugin } from "./definitions";
 
-const { NativeMarket: NM } = Plugins;
+const { NativeMarket: NativeMarketPlugin } = Plugins;
 
 export class NativeMarket implements NMPlugin {
-    openStoreListing(options: { appId: string; }): Promise<void> {
-        return NM.openStoreListing(options);
-    }
+  openStoreListing(options: { appId: string }): Promise<void> {
+    return NativeMarketPlugin.openStoreListing(options);
+  }
 
-    openDevPage(options: { devId: string; }): Promise<void> {
-        return NM.openDevPage(options);
-    }
+  openDevPage(options: { devId: string }): Promise<void> {
+    return NativeMarketPlugin.openDevPage(options);
+  }
 
-    openCollection(options: { name: string; }): Promise<void> {
-        return NM.openCollection(options);
-    }
+  openCollection(options: { name: string }): Promise<void> {
+    return NativeMarketPlugin.openCollection(options);
+  }
 
-    openEditorChoicePage(options: { editorChoice: string; }): Promise<void> {
-        return NM.openEditorChoicePage(options);
-    }
-    
-    search(options: { terms: string; }): Promise<void> {
-        return NM.search(options);
-    }
+  openEditorChoicePage(options: { editorChoice: string }): Promise<void> {
+    return NativeMarketPlugin.openEditorChoicePage(options);
+  }
 
+  search(options: { terms: string }): Promise<void> {
+    return NativeMarketPlugin.search(options);
+  }
 }
--- src/web.ts
+++ src/web.ts
@@ -1,45 +1,43 @@
-import { WebPlugin } from '@capacitor/core';
-import { NativeMarketPlugin } from './definitions';
+import { WebPlugin } from "@capacitor/core";
+import { NativeMarketPlugin } from "./definitions";
 
 export class NativeMarketWeb extends WebPlugin implements NativeMarketPlugin {
   constructor() {
     super({
-      name: 'NativeMarket',
-      platforms: ['web']
+      name: "NativeMarket",
+      platforms: ["web"],
     });
   }
 
-  openStoreListing(options: { appId: string; }): Promise<void> {
-    console.warn(options);
-    throw new Error("Method not implemented.");
-  }
-  
-  openDevPage(options: { devId: string; }): Promise<void> {
+  openStoreListing(options: { appId: string }): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
 
-  openCollection(options: { name: string; }): Promise<void> {
+  openDevPage(options: { devId: string }): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
 
-  openEditorChoicePage(options: { editorChoice: string; }): Promise<void> {
+  openCollection(options: { name: string }): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
 
-  search(options: { terms: string; }): Promise<void> {
+  openEditorChoicePage(options: { editorChoice: string }): Promise<void> {
     console.warn(options);
     throw new Error("Method not implemented.");
   }
 
-  
+  search(options: { terms: string }): Promise<void> {
+    console.warn(options);
+    throw new Error("Method not implemented.");
+  }
 }
 
 const NativeMarket = new NativeMarketWeb();
 
 export { NativeMarket };
 
-import { registerWebPlugin } from '@capacitor/core';
+import { registerWebPlugin } from "@capacitor/core";
 registerWebPlugin(NativeMarket);
