dae4bfa6667ab74c216334ec21acd6b2edbed781
jcesarmobile <jcesarmobile@gmail.com>
2024-04-18 19:25:06 +0200

    refactor!: Remove unused and deprecated code (#23)

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,21 +9,14 @@ import Capacitor
 public class NativeMarket: CAPPlugin {
     
     @objc func openStoreListing(_ call: CAPPluginCall) {
-        if call.hasOption("appId") {
-            let appId = call.getString("appId")
-            
-            let url = "itms-apps://itunes.apple.com/app/" + appId!
+        if let appId = call.getString("appId") {
+            let url = "itms-apps://itunes.apple.com/app/" + appId
             let appUrl = URL(string: url)
             
             DispatchQueue.main.async {
                 if UIApplication.shared.canOpenURL(appUrl!) {
-                    if #available(iOS 10.0, *) {
-                        UIApplication.shared.open(appUrl!, options: [:]) { (success) in
-                            call.success()
-                        }
-                    } else {
-                        UIApplication.shared.openURL(appUrl!)
-                        call.success()
+                    UIApplication.shared.open(appUrl!, options: [:]) { (success) in
+                        call.resolve()
                     }
                 }
             }
@@ -33,32 +26,25 @@ public class NativeMarket: CAPPlugin {
     }
     
     @objc func openDevPage(_ call: CAPPluginCall) {
-        call.success() // TODO: Implement
+        call.resolve() // TODO: Implement
     }
     
     @objc func openCollection(_ call: CAPPluginCall) {
-        call.success() // TODO: Implement
+        call.resolve() // TODO: Implement
     }
     
     @objc func openEditorChoicePage(_ call: CAPPluginCall) {
-        call.success() // TODO: Implement
+        call.resolve() // TODO: Implement
     }
     
     @objc func search(_ call: CAPPluginCall) {
-        if call.hasOption("terms") {
-            let terms = call.getString("terms")
-            
-            let url = "itms-apps://itunes.apple.com/search?term=" + terms!
+        if let terms = call.getString("terms") {
+            let url = "itms-apps://itunes.apple.com/search?term=" + terms
             let appUrl = URL(string: url)
             
             if UIApplication.shared.canOpenURL(appUrl!) {
-                if #available(iOS 10.0, *) {
-                    UIApplication.shared.open(appUrl!, options: [:]) { (success) in
-                        call.success()
-                    }
-                } else {
-                    UIApplication.shared.openURL(appUrl!)
-                    call.success()
+                UIApplication.shared.open(appUrl!, options: [:]) { (success) in
+                    call.resolve()
                 }
             }
         } else {
