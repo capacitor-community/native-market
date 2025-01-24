00489145cca3153b235e9b0751899ffa92569a93
jcesarmobile <jcesarmobile@gmail.com>
2025-01-24 19:12:22 +0100

    fmt

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -7,15 +7,15 @@ import Capacitor
  */
 @objc(NativeMarket)
 public class NativeMarket: CAPPlugin {
-    
+
     @objc func openStoreListing(_ call: CAPPluginCall) {
         if let appId = call.getString("appId") {
             let url = "itms-apps://itunes.apple.com/app/" + appId
             let appUrl = URL(string: url)
-            
+
             DispatchQueue.main.async {
                 if UIApplication.shared.canOpenURL(appUrl!) {
-                    UIApplication.shared.open(appUrl!, options: [:]) { (success) in
+                    UIApplication.shared.open(appUrl!, options: [:]) { (_) in
                         call.resolve()
                     }
                 }
@@ -24,26 +24,26 @@ public class NativeMarket: CAPPlugin {
             call.reject("appId is missing")
         }
     }
-    
+
     @objc func openDevPage(_ call: CAPPluginCall) {
         call.resolve() // TODO: Implement
     }
-    
+
     @objc func openCollection(_ call: CAPPluginCall) {
         call.resolve() // TODO: Implement
     }
-    
+
     @objc func openEditorChoicePage(_ call: CAPPluginCall) {
         call.resolve() // TODO: Implement
     }
-    
+
     @objc func search(_ call: CAPPluginCall) {
         if let terms = call.getString("terms") {
             let url = "itms-apps://itunes.apple.com/search?term=" + terms
             let appUrl = URL(string: url)
-            
+
             if UIApplication.shared.canOpenURL(appUrl!) {
-                UIApplication.shared.open(appUrl!, options: [:]) { (success) in
+                UIApplication.shared.open(appUrl!, options: [:]) { (_) in
                     call.resolve()
                 }
             }
