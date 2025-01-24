b7f44746190759e397316b21af87b8a35ca10044
jcesarmobile <jcesarmobile@gmail.com>
2025-01-24 19:06:37 +0100

    chore: add lint/prettier scripts (#33)

--- android/src/main/java/com/getcapacitor/community/nativemarket/NativeMarket.java
+++ android/src/main/java/com/getcapacitor/community/nativemarket/NativeMarket.java
@@ -11,120 +11,106 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "NativeMarket")
 public class NativeMarket extends Plugin {
 
-  @PluginMethod
-  public void openStoreListing(PluginCall call) {
-    try {
-      if (call.hasOption("appId")) {
-        String appId = call.getString("appId");
+    @PluginMethod
+    public void openStoreListing(PluginCall call) {
+        try {
+            if (call.hasOption("appId")) {
+                String appId = call.getString("appId");
 
-        Context context = this.bridge.getActivity().getApplicationContext();
-        Intent intent = new Intent(
-          Intent.ACTION_VIEW,
-          Uri.parse("market://details?id=" + appId)
-        );
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+                Context context = this.bridge.getActivity().getApplicationContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appId));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
 
-        call.resolve();
-      } else {
-        call.reject("appId is missing");
-      }
-    } catch (Exception ex) {
-      call.reject(ex.getLocalizedMessage());
+                call.resolve();
+            } else {
+                call.reject("appId is missing");
+            }
+        } catch (Exception ex) {
+            call.reject(ex.getLocalizedMessage());
+        }
     }
-  }
 
-  @PluginMethod
-  public void openDevPage(PluginCall call) {
-    try {
-      if (call.hasOption("devId")) {
-        String devId = call.getString("devId");
+    @PluginMethod
+    public void openDevPage(PluginCall call) {
+        try {
+            if (call.hasOption("devId")) {
+                String devId = call.getString("devId");
 
-        Context context = this.getContext();
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(
-          Uri.parse("https://play.google.com/store/apps/dev?id=" + devId)
-        );
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+                Context context = this.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/dev?id=" + devId));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
 
-        call.resolve();
-      } else {
-        call.reject("devId is missing");
-      }
-    } catch (Exception ex) {
-      call.reject(ex.getLocalizedMessage());
+                call.resolve();
+            } else {
+                call.reject("devId is missing");
+            }
+        } catch (Exception ex) {
+            call.reject(ex.getLocalizedMessage());
+        }
     }
-  }
 
-  @PluginMethod
-  public void openCollection(PluginCall call) {
-    try {
-      if (call.hasOption("name")) {
-        String name = call.getString("name");
+    @PluginMethod
+    public void openCollection(PluginCall call) {
+        try {
+            if (call.hasOption("name")) {
+                String name = call.getString("name");
 
-        Context context = this.getContext();
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(
-          Uri.parse("https://play.google.com/store/apps/collection/" + name)
-        );
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+                Context context = this.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/collection/" + name));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
 
-        call.resolve();
-      } else {
-        call.reject("name is missing");
-      }
-    } catch (Exception ex) {
-      call.reject(ex.getLocalizedMessage());
+                call.resolve();
+            } else {
+                call.reject("name is missing");
+            }
+        } catch (Exception ex) {
+            call.reject(ex.getLocalizedMessage());
+        }
     }
-  }
 
-  @PluginMethod
-  public void openEditorChoicePage(PluginCall call) {
-    try {
-      if (call.hasOption("editorChoice")) {
-        String editorChoice = call.getString("editorChoice");
+    @PluginMethod
+    public void openEditorChoicePage(PluginCall call) {
+        try {
+            if (call.hasOption("editorChoice")) {
+                String editorChoice = call.getString("editorChoice");
 
-        Context context = this.getContext();
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(
-          Uri.parse(
-            "https://play.google.com/store/apps/topic?id=" + editorChoice
-          )
-        );
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+                Context context = this.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://play.google.com/store/apps/topic?id=" + editorChoice));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
 
-        call.resolve();
-      } else {
-        call.reject("editorChoice is missing");
-      }
-    } catch (Exception ex) {
-      call.reject(ex.getLocalizedMessage());
+                call.resolve();
+            } else {
+                call.reject("editorChoice is missing");
+            }
+        } catch (Exception ex) {
+            call.reject(ex.getLocalizedMessage());
+        }
     }
-  }
 
-  @PluginMethod
-  public void search(PluginCall call) {
-    try {
-      if (call.hasOption("terms")) {
-        String terms = call.getString("terms");
+    @PluginMethod
+    public void search(PluginCall call) {
+        try {
+            if (call.hasOption("terms")) {
+                String terms = call.getString("terms");
 
-        Context context = this.getContext();
-        Intent intent = new Intent(
-          Intent.ACTION_VIEW,
-          Uri.parse("market://search?q=" + terms)
-        );
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+                Context context = this.getContext();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://search?q=" + terms));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
 
-        call.resolve();
-      } else {
-        call.reject("terms is missing");
-      }
-    } catch (Exception ex) {
-      call.reject(ex.getLocalizedMessage());
+                call.resolve();
+            } else {
+                call.reject("terms is missing");
+            }
+        } catch (Exception ex) {
+            call.reject(ex.getLocalizedMessage());
+        }
     }
-  }
 }
--- src/web.ts
+++ src/web.ts
@@ -1,32 +1,33 @@
-import { WebPlugin } from "@capacitor/core";
-import { NativeMarketPlugin } from "./definitions";
+import { WebPlugin } from '@capacitor/core';
+
+import type { NativeMarketPlugin } from './definitions';
 
 export class NativeMarketWeb extends WebPlugin implements NativeMarketPlugin {
   constructor() {
     super({
-      name: "NativeMarket",
-      platforms: ["web"],
+      name: 'NativeMarket',
+      platforms: ['web'],
     });
   }
 
   openStoreListing(_options: { appId: string }): Promise<void> {
-    throw this.unimplemented("Not implemented on web.");
+    throw this.unimplemented('Not implemented on web.');
   }
 
   openDevPage(_options: { devId: string }): Promise<void> {
-    throw this.unimplemented("Not implemented on web.");
+    throw this.unimplemented('Not implemented on web.');
   }
 
   openCollection(_options: { name: string }): Promise<void> {
-    throw this.unimplemented("Not implemented on web.");
+    throw this.unimplemented('Not implemented on web.');
   }
 
   openEditorChoicePage(_options: { editorChoice: string }): Promise<void> {
-    throw this.unimplemented("Not implemented on web.");
+    throw this.unimplemented('Not implemented on web.');
   }
 
   search(_options: { terms: string }): Promise<void> {
-    throw this.unimplemented("Not implemented on web.");
+    throw this.unimplemented('Not implemented on web.');
   }
 }
 
