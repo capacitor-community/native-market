a4634ad58c66abe7df9545bf8ae443a1fa3dc75a
jcesarmobile <jcesarmobile@gmail.com>
2025-03-28 20:15:13 +0100

    missing package.swift file

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CapacitorCommunityNativeMarket",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "CapacitorCommunityNativeMarket",
+            targets: ["NativeMarketPlugin"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+    ],
+    targets: [
+        .target(
+            name: "NativeMarketPlugin",
+            dependencies: [
+                .product(name: "Capacitor", package: "capacitor-swift-pm"),
+                .product(name: "Cordova", package: "capacitor-swift-pm")
+            ],
+            path: "ios/Sources/NativeMarketPlugin"),
+        .testTarget(
+            name: "NativeMarketPluginTests",
+            dependencies: ["NativeMarketPlugin"],
+            path: "ios/Tests/NativeMarketPluginTests")
+    ]
+)
